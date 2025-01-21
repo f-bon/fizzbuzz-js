0119f535c960f6ef76e9db967d5853011cd6d45c
Faith Martin <faith.martin@softwire.com>
2025-01-21 10:43:02 +0000

    added Fizz and Fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,10 +10,16 @@ function fizzbuzz() {
     console.log('hello world')
 
     for(let i = 1; i < 101; i++){
-
-        if (i % 3 === 0) {
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz")
+        }
+        if (i % 3 === 0 && i % 5 !== 0) {
             console.log("Fizz")
-        } else {
+        } 
+        if (i % 5 === 0 && i % 3 !== 0) {
+            console.log("Buzz")
+        }
+        if (i % 5 !== 0 && i % 3 !== 0) {
         console.log(i)
     }
     }
