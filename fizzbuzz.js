6e65f4f71c297c4a26221e9332d514879cb40748
Faith Martin <faith.martin@softwire.com>
2025-01-21 10:27:13 +0000

    added fizz for multiples of 3

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -9,8 +9,13 @@ function fizzbuzz() {
 
     console.log('hello world')
 
-    for(let i = 0; i < 100; i++){
-        console.log(i + 1)
+    for(let i = 1; i < 101; i++){
+
+        if (i % 3 === 0) {
+            console.log("Fizz")
+        } else {
+        console.log(i)
+    }
     }
 
     
